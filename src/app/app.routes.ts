1c5d0cfeb680c20fd74e138bdab2e3e56fad0f54
wixhub <rublin@gmx.de>
2026-08-14 18:11:49 +0200

    feat: add HttpClient provider and define application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
